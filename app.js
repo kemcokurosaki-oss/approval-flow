23e580e8254fe4c0ddd746e422709f548a889cf0
黒崎 <e-kurosaki@kusakabe.com>
2026-07-29 10:47:50 +0900

    Auto-commit: Claude Code changes

--- app.js
+++ app.js
@@ -1079,11 +1079,12 @@ function setProgressFilterShipAfter(checked) {
 }
 
 function matchesPrefix(num, prefix) {
-    if (prefix === '3')  return /^3\d/.test(num);
-    if (prefix === '4')  return /^4\d/.test(num);
-    if (prefix === '3C') return /^3C/i.test(num);
-    if (prefix === '4C') return /^4C/i.test(num);
-    if (prefix === 'D')  return /^D/i.test(num);
+    if (prefix === '3')    return /^3\d/.test(num);
+    if (prefix === '4')    return /^4\d/.test(num);
+    if (prefix === '3C')   return /^3C/i.test(num);
+    if (prefix === '4C')   return /^4C/i.test(num);
+    if (prefix === 'D')    return /^D/i.test(num);
+    if (prefix === '2000') return is2000sSeries(num);
     return true;
 }
 
